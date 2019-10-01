86b51a8f4b65856ff843e85b251823ea8ceca63b
Marcelo Corrêa <marcelocorrea229@gmail.com>
2019-10-01 12:32:02 -0300

    ...

--- database/migrations/2019_01_10_000000_create_laravel_logs_table.php
+++ database/migrations/2019_01_10_000000_create_laravel_logs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class LaravelLogsTable extends Migration
+class CreateLaravelLogsTable extends Migration
 {
 
     /**
